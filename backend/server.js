b3b03d81baf4f62c9033f8f6fad3251b2cd6acbb
Miheer Gautam <miheergautam04@gmail.com>
2025-02-10 17:12:42 +0530

    Partial project complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -8,8 +8,13 @@ connectDB();
 
 app.use(express.json());
 
-app.use('/api/v1/auth', require('./routes/authRoutes'));
-app.use('/api/v1/resources', require('./routes/resourceRoutes'));
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Jarurat Care Backend API is Running!");
+});
+
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/resources", require("./routes/resourceRoutes"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
